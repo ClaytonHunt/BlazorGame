604cbbdbf307b48633d62db86a403981e33b4cdf
Clayton Hunt <claytonhunt@outlook.com>
2021-05-05 21:14:09 -0400

    I did this earlier, yep

---
+++ BlazorGame.Library/Graphics/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorGame.Framework.Graphics
+{
+    public struct Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
